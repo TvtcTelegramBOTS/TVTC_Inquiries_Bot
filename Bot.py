88fe329535c1c80d87a3c4274e7b65c93d8ee798
TvtcTelegramBOTS <carlossantos1111985@gmail.com>
2025-12-04 09:41:10 +0300

    تحديث ملفات البوت

--- Bot.py
+++ Bot.py
@@ -720,7 +720,7 @@ def build_main_keyboard(student_id: str):
 
     # نضيف زر الشهادات فقط إذا له شهادة
     if has_certificate:
-        keyboard.insert(1, [KeyboardButton("📜 شهادات البرامج")])
+        keyboard.insert(1, [KeyboardButton("📜 شهادات البرامج المساندة")])
 
     return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
